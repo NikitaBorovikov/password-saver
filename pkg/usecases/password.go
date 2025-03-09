c361b57b53708e4dc16fbe15a159d29ca6fb6df2
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-03-09 16:50:28 +0300

    cleaned up getAll password usecase

--- pkg/usecases/password.go
+++ pkg/usecases/password.go
@@ -54,21 +54,9 @@ func (uc *PasswordUseCase) GetAll(userID int64) ([]dto.PasswordResponse, error) 
 		return nil, err
 	}
 
-	passwordResponse := []dto.PasswordResponse{}
-
-	for _, elem := range userPasswords {
-		var userPassword dto.PasswordResponse
-		password, _ := base64.StdEncoding.DecodeString(elem.EncPassword)
-		service, _ := base64.StdEncoding.DecodeString(elem.EncService)
-		userPassword.Password, err = encryption.Decrypt((password), []byte(uc.cfg.EncPasswordKey))
-		if err != nil {
-			return nil, err
-		}
-		userPassword.Service, err = encryption.Decrypt([]byte(service), []byte(uc.cfg.EncServiceKey))
-		if err != nil {
-			return nil, err
-		}
-		passwordResponse = append(passwordResponse, userPassword)
+	passwordResponse, err := uc.makePasswordResponse(userPasswords)
+	if err != nil {
+		return nil, err
 	}
 
 	return passwordResponse, nil
@@ -86,6 +74,30 @@ func (uc *PasswordUseCase) Delete(passwordID string) error {
 	return nil
 }
 
+func (uc *PasswordUseCase) makePasswordResponse(userPasswords []model.Password) ([]dto.PasswordResponse, error) {
+	passwordResponse := make([]dto.PasswordResponse, 0, len(userPasswords))
+	var err error
+
+	for _, elem := range userPasswords {
+
+		var userPassword dto.PasswordResponse
+
+		userPassword.Password, err = decodeData(elem.EncPassword, uc.cfg.EncPasswordKey)
+		if err != nil {
+			return nil, err
+		}
+
+		userPassword.Service, err = decodeData(elem.EncService, uc.cfg.EncServiceKey)
+		if err != nil {
+			return nil, err
+		}
+
+		passwordResponse = append(passwordResponse, userPassword)
+	}
+
+	return passwordResponse, nil
+}
+
 func validateForSavePassword(req *dto.SavePasswordRequest) error {
 	validate := validator.New()
 	if err := validate.Struct(req); err != nil {
@@ -101,4 +113,18 @@ func encryptData(data string, encKey string) (string, error) {
 	}
 
 	return encData, nil
+}
+
+func decodeData(encData string, encKey string) (string, error) {
+	encDataInByte, err := base64.StdEncoding.DecodeString(encData)
+	if err != nil {
+		return "", fmt.Errorf("failed to decode string by byte: %v", err)
+	}
+
+	plainData, err := encryption.Decrypt(encDataInByte, []byte(encKey))
+	if err != nil {
+		return "", fmt.Errorf("failed to decrypt data: %v", err)
+	}
+
+	return plainData, nil
 }
