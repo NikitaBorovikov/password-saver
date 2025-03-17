145fe3debd7686aa9b73ee63cffc60b907730ff8
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-03-17 17:15:22 +0300

    errors were sorted

--- pkg/errors/apperrors.go
+++ pkg/errors/apperrors.go
@@ -2,18 +2,38 @@ package apperrors
 
 import "errors"
 
+// Database errors
 var (
-	ErrUserNotFound     = errors.New("user not found")
-	ErrDatabaseInternal = errors.New("internal database error")
+	ErrUserNotFound     = errors.New("failed to find user")
 	ErrDuplicateUser    = errors.New("user already exists")
+	ErrDatabaseInternal = errors.New("internal database error")
+)
+
+// Validation errors
+var (
 	ErrValidateUser     = errors.New("failed to validate user data")
+	ErrValidatePassword = errors.New("failed to validate password")
+)
+
+// Auth errors
+var (
+	ErrNotAuthenticated = errors.New("user is not authenticated")
 	ErrHashPassword     = errors.New("failed to hash password")
 	ErrComparePasswords = errors.New("incorrect password")
-	ErrDecodeRequest    = errors.New("failed to decode request")
-	ErrServerInternal   = errors.New("internal servr error")
-	ErrNotAuthenticated = errors.New("user is not authenticated")
-	ErrInvalidURLParam  = errors.New("invalid URL param")
+)
 
+// Request errors
+var (
+	ErrDecodeRequest   = errors.New("failed to decode request")
+	ErrInvalidURLParam = errors.New("invalid URL param")
+)
+
+// Password errors
+var (
 	ErrPasswordNotExists = errors.New("password not exists")
-	ErrValidatePassword  = errors.New("failed to validate password")
+)
+
+// Server errors
+var (
+	ErrServerInternal = errors.New("internal server error")
 )
