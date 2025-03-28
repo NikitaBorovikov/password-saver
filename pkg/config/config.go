29c584c5809d675a2cc32e76b3843ed341d9cc84
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-03-28 20:11:35 +0300

    added docker-compose

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"github.com/ilyakaznacheev/cleanenv"
-	"github.com/joho/godotenv"
 )
 
 type (
@@ -47,9 +46,6 @@ func Init() (*Config, error) {
 }
 
 func ReadFromEnv(cfg *Config) error {
-	if err := godotenv.Load(); err != nil {
-		return err
-	}
 
 	if err := cleanenv.ReadEnv(&cfg.Postgres); err != nil {
 		return err
