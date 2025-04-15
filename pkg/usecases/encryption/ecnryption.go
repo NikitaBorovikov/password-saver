0e8fdad55f5b0d2f3a6df58b5e219f1d3f466b2e
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-04-15 20:31:53 +0300

    added comments on encryption and decryption

--- pkg/usecases/encryption/ecnryption.go
+++ pkg/usecases/encryption/ecnryption.go
@@ -10,45 +10,55 @@ import (
 )
 
 func Encrypt(plainText, key []byte) (string, error) {
+	// Creating an AES cipher with a key 32 bytes
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
 
+	// Generating a random IV with a length of 12 bytes
 	iv := make([]byte, 12)
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
 		return "", err
 	}
 
+	// Returns the block cipher, converted to GCM mode
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return "", err
 	}
 
+	// Encrypt the data
 	ciphertext := aesgcm.Seal(nil, iv, plainText, nil)
 
+	// Encoding the result in Base64 (IV + ciphertext)
 	encText := base64.StdEncoding.EncodeToString(append(iv, ciphertext...))
 
 	return encText, nil
 }
 
 func Decrypt(ciphertext []byte, key []byte) (string, error) {
+	// Creating an AES cipher with a key 32 bytes
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
 
+	// Checking the length of the data (there must be at least a IV)
 	if len(ciphertext) < 12 {
 		return "", errors.New("ciphertext too short")
 	}
+	// Extracting the IV
 	iv := ciphertext[:12]
 	ciphertext = ciphertext[12:]
 
+	// Returns the block cipher, converted to GCM mode
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return "", err
 	}
 
+	//Decrypting and verifying authentication
 	plainText, err := aesgcm.Open(nil, iv, ciphertext, nil)
 	if err != nil {
 		return "", err
