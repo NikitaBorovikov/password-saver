b32fd8b49057e135bca4256bc197209502bb9fcb
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2026-02-01 20:08:17 +0300

    remove redundant dependency godotenv in favor of cleanenv

--- pkg/infrastructure/config/config.go
+++ pkg/infrastructure/config/config.go
@@ -5,7 +5,12 @@ import (
 	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
-	"github.com/joho/godotenv"
+)
+
+const (
+	configYAMLPath    = "config/config.yml"
+	configProdENVPath = ".prod.env"
+	configDevENVPath  = ".dev.env"
 )
 
 type (
@@ -23,7 +28,7 @@ type (
 
 	Postgres struct {
 		Host     string `env:"PG_HOST"`
-		Port     int64  `env:"PG_PORT"`
+		Port     int    `env:"PG_PORT"`
 		User     string `env:"PG_USER"`
 		Password string `env:"PG_PASSWORD"`
 		Name     string `env:"PG_NAME"`
@@ -54,43 +59,28 @@ type (
 func Init() (*Config, error) {
 	var cfg Config
 
-	if err := cleanenv.ReadConfig("config/config.yml", &cfg); err != nil {
+	if err := cleanenv.ReadConfig(configYAMLPath, &cfg); err != nil {
 		return nil, err
 	}
 
-	if err := ReadFromEnv(&cfg); err != nil {
+	if err := readFromEnv(&cfg); err != nil {
 		return nil, err
 	}
-
 	return &cfg, nil
 }
 
-func ReadFromEnv(cfg *Config) error {
+func readFromEnv(cfg *Config) error {
 	env := os.Getenv("APP_ENV")
-	if env == "" {
-		env = "dev"
-	}
 
-	//if not prod, then we take the data from the .env.dev file.
-	//if prod, then from the environment variables
-	if env == "dev" {
-		if err := godotenv.Load(".env.dev"); err != nil {
+	switch env {
+	case "prod":
+		if err := cleanenv.ReadConfig(configProdENVPath, cfg); err != nil {
+			return err
+		}
+	default:
+		if err := cleanenv.ReadConfig(configDevENVPath, cfg); err != nil {
 			return err
 		}
 	}
-
-	if err := cleanenv.ReadEnv(&cfg.Postgres); err != nil {
-		return err
-	}
-
-	if err := cleanenv.ReadEnv(&cfg.Http); err != nil {
-		return err
-	}
-
-	if err := cleanenv.ReadEnv(&cfg.Telegram); err != nil {
-		return err
-	}
-
-	err := cleanenv.ReadEnv(&cfg.EncryptKeys)
-	return err
+	return nil
 }
